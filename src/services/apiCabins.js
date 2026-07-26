a7b78e368a5f8a413f5790ee1f0c2c2372f0589c
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-07-26 16:16:46 +0300

    delete old version

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -40,15 +40,14 @@ export async function apiCreateCabin(newCabin) {
 
 export async function apiEditCabin(newCabin, id, editImage) {
   const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
-  // Если hasImagePath === true то это редактирование текста
-  // Если hasImagePath === falsе то это редактирование текста и загрузка новой картинки
-
   let query = supabase.from('cabins');
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
   const imagePath = hasImagePath
     ? newCabin.image
     : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
+  // Если hasImagePath === true то это редактирование текста
+  // Если hasImagePath === falsе то это редактирование текста и загрузка новой картинки
   if (hasImagePath) {
     query = query.update(newCabin).eq('id', id);
   } else {
@@ -84,49 +83,4 @@ export async function apiDeleteCabin(id, image) {
     console.error(error);
     throw new Error('Cabin could not be deleted');
   }
-}
-
-// Старая версия
-export async function createEditCabin(newCabin, id) {
-  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
-  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
-  const imagePath = hasImagePath
-    ? newCabin.image
-    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
-
-  // 1. Создание/редактирование cabin
-  let query = supabase.from('cabins');
-
-  // a) Создание
-  if (!id) {
-    query = query.insert([{ ...newCabin, image: imagePath }]);
-  }
-
-  // b) Редактирование
-  if (id) {
-    query = query.update({ ...newCabin, image: imagePath }).eq('id', id);
-  }
-
-  const { data, error } = await query.select().single();
-
-  if (error) {
-    console.error(error);
-    throw new Error('Cabin could not be created');
-  }
-
-  // 2. Загрузка картинки
-  if (!hasImagePath) {
-    const { error: storageError } = await supabase.storage
-      .from('cabin-images')
-      .upload(imageName, newCabin.image);
-
-    // 3. Удаление cabin если была ошибка загрузки картинки
-    if (storageError && !id) {
-      await supabase.from('cabins').delete().eq('id', data.id);
-      console.error(storageError);
-      throw new Error('Cabin image could not be uploaded and the cabin was not created');
-    }
-  }
-
-  return data;
 }
